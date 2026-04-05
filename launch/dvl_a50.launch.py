393318360747122cd801e3d4402fb1b6a1c8e023
ranbier <kimss6240@gmail.com>
2026-04-05 21:43:42 +0900

    launch edited

--- launch/dvl_a50.launch.py
+++ launch/dvl_a50.launch.py
@@ -5,17 +5,17 @@ import launch_ros.actions
 
 
 def generate_launch_description():
-    launch.actions.DeclareLaunchArgument('ip_address', default_value='192.168.194.95'),
-    dvl_a50 = launch_ros.actions.Node(
-        package='dvl_a50', 
-        executable='dvl_a50_sensor', 
-        parameters=[{'dvl_ip_address': launch.substitutions.LaunchConfiguration('ip_address')}],
-        output='screen')
-
-
     return launch.LaunchDescription([
-        dvl_a50,
+        launch.actions.DeclareLaunchArgument('ip_address', default_value='192.168.194.95'),
+        launch.actions.DeclareLaunchArgument('velocity_frame_id', default_value='dvl'),
+        launch.actions.DeclareLaunchArgument('position_frame_id', default_value='dvl'),
+        launch_ros.actions.Node(
+            package='dvl_a50',
+            executable='dvl_a50_sensor',
+            parameters=[{
+                'dvl_ip_address': launch.substitutions.LaunchConfiguration('ip_address'),
+                'velocity_frame_id': launch.substitutions.LaunchConfiguration('velocity_frame_id'),
+                'position_frame_id': launch.substitutions.LaunchConfiguration('position_frame_id'),
+            }],
+            output='screen'),
     ])
-
-
-       
